cb566e3124171b96fb41789bdb1ccf6b67111613
Daniel Davis <daniel.davis@excella.com>
2016-10-26 16:17:18 -0400

    Added a test for wine, updated CheckoutMachine

--- checkout_machine.rb
+++ checkout_machine.rb
@@ -2,8 +2,10 @@ class CheckoutMachine
   def scan(sku)
     if sku == 123
       @total = 200
-    else
+    elsif sku == 456
       @total = 100
+    else
+      @total = 1000
     end
   end
 
--- checkout_machine_test.rb
+++ checkout_machine_test.rb
@@ -23,4 +23,15 @@ class CheckoutMachineTest < Minitest::Test
     # Assert
     assert_equal 100, cm.total
   end
+
+  def test_scan_wine_expects_total_1000
+    # Arrange
+    cm = CheckoutMachine.new
+
+    # Act
+    cm.scan(789)
+
+    # Assert
+    assert_equal 1000, cm.total
+  end
 end
