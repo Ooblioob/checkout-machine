61a792704901db8420b43a5a13c59fd81726be16
Daniel Davis <daniel.davis@excella.com>
2016-10-26 16:23:17 -0400

    Refactoring tests to use setup

--- checkout_machine_test.rb
+++ checkout_machine_test.rb
@@ -2,47 +2,49 @@ require 'minitest/autorun'
 require_relative 'checkout_machine'
 
 class CheckoutMachineTest < Minitest::Test
+  def setup
+    @cm = CheckoutMachine.new
+  end
+
   def test_scan_chips_expects_total_200
     # Arrange
-    cm = CheckoutMachine.new
 
     # Act
-    cm.scan(123)
+    @cm.scan(123)
 
     # Assert
-    assert_equal 200, cm.total
+    assert_equal 200, @cm.total
   end
 
   def test_scan_salsa_expects_total_100
     # Arrange
-    cm = CheckoutMachine.new
 
     # Act
-    cm.scan(456)
+    @cm.scan(456)
 
     # Assert
-    assert_equal 100, cm.total
+    assert_equal 100, @cm.total
   end
 
   def test_scan_wine_expects_total_1000
     # Arrange
-    cm = CheckoutMachine.new
 
     # Act
-    cm.scan(789)
+    @cm.scan(789)
 
     # Assert
-    assert_equal 1000, cm.total
+    assert_equal 1000, @cm.total
   end
 
   def test_scan_cigarettes_expects_total_500
     # Arrange
-    cm = CheckoutMachine.new
 
     # Act
-    cm.scan(111)
+    @cm.scan(111)
 
     # Assert
-    assert_equal 500, cm.total
+    assert_equal 500, @cm.total
   end
+
+
 end
